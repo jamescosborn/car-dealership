1cf500759e4822279c12a69e245421cb9bd31fd8
Emily Wells Jimenez, James Osborn and Kimberly Bordon <emily+james+kimberly@>
2017-10-10 11:52:34 -0700

    Display entire car lot.

--- Controller/HomeController.cs
+++ Controller/HomeController.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
+using CarDealership.Models;
 
 namespace CarDealership.Controllers
 {
@@ -10,6 +11,29 @@ namespace CarDealership.Controllers
     {
       return View();
     }
-    
+
+    [HttpPost("/car/create")]
+    public ActionResult CreateCar()
+    {
+      string newMakeModel = Request.Form["new-makemodel"];
+      string newYear = Request.Form["new-year"];
+      string newPrice = Request.Form["new-price"];
+      string newMileage = Request.Form["new-mileage"];
+      Car newCar = new Car (newMakeModel, newYear, newPrice, newMileage);
+      newCar.Save();
+      return View(newCar);
+    }
+    [Route("/CarLotList")]
+    public ActionResult CarLotList()
+    {
+      List<Car> allCars = Car.GetAll();
+      return View(allCars);
+    }
+    [HttpPost("/CarList/clear")]
+    public ActionResult CarListClear()
+    {
+      Car.ClearAll();
+      return View();
+    }
   }
 }
--- Models/Car.cs
+++ Models/Car.cs
@@ -6,18 +6,64 @@ namespace CarDealership.Models
   public class Car
   {
     private string _makeModel;
-    private int _year;
-    private int _price;
-    private int _miles;
+    private string _year;
+    private string _price;
+    private string _mileage;
 
     private static List<Car> _CarLot = new List<Car> {};
 
-    public Car (string makeModel, int year, int price, int miles)
+    public Car (string makeModel, string year, string price, string mileage)
     {
       _makeModel = makeModel;
       _year = year;
       _price = price;
-      _miles = miles;
+      _mileage = mileage;
+    }
+
+    public void Save()
+    {
+      _CarLot.Add(this);
+    }
+
+    public string GetMakeModel()
+    {
+      return _makeModel;
+    }
+    public void SetMakeModel(string newMakeModel)
+    {
+      _makeModel = newMakeModel;
+    }
+    public string GetYear()
+    {
+      return _year;
+    }
+    public void SetYear(string newYear)
+    {
+      _year = newYear;
+    }
+    public string GetPrice()
+    {
+      return _price;
+    }
+    public void SetPrice(string newPrice)
+    {
+      _price = newPrice;
+    }
+    public string GetMileage()
+    {
+      return _mileage;
+    }
+    public void SetMileage(string newMileage)
+    {
+      _mileage = newMileage;
+    }
+    public static List<Car> GetAll()
+    {
+      return _CarLot;
+    }
+    public static void ClearAll()
+    {
+      _CarLot.Clear();
     }
   }
 
